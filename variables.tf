8ee97436f140d1f38053b1a15774558b597dba48
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-13 00:36:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,13 +7,3 @@ variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
 }
-
-variable "cluster_endpoint" {
-  description = "EKS cluster API endpoint"
-  type        = string
-}
-
-variable "cluster_ca" {
-  description = "EKS cluster CA certificate"
-  type        = string
-}
