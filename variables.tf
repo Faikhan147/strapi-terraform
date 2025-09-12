774ee0f69a631008aa8d3101627490853f5daa53
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-12 23:58:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
 variable "cluster_endpoint" {
   description = "EKS cluster API endpoint"
   type        = string
@@ -5,10 +15,5 @@ variable "cluster_endpoint" {
 
 variable "cluster_ca" {
   description = "EKS cluster CA certificate"
-  type        = string
-}
-
-variable "cluster_token" {
-  description = "EKS Bearer token"
   type        = string
 }
